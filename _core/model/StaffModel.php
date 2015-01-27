9814b1a2dd23a2e57cbe250b2ebce48479da59f7
joe_godcares <joe_godcares@yahoo.com>
2015-01-27 12:22:08 +0100

    Staff model class for adding and querying staff added

---
+++ _core/model/StaffModel.php
@@ -0,0 +1,30 @@
+<?php
+
+class StaffModel extends BaseModel{
+    public static function addStaff(){
+
+    }
+
+    public static function getStaff(){
+
+    }
+
+    public static function addProfile($userId, $surname, $firstName, $middleName,
+                                      $workAddress, $homeAddress, $telephone,
+                                      $birthDate, $sex, $height, $weight){
+
+    }
+
+    public static function getProfile($regNo){
+
+    }
+
+    public static function addAuthInfo($regNo, $passcode, $userType){
+
+    }
+
+    public static function editPasscode($regNo, $passcode){
+
+    }
+
+}
