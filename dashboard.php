f9325b618609997321e80c5bc80c3a1421a67473
moses adebayo <mos.adebayo@gmail.com>
2015-01-27 14:35:48 +0100

    user dashboard

---
+++ dashboard.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PMS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/master.css" rel="stylesheet">
+
+    <script src="js/bootstrap/jquery-1.10.0.min.js"></script>
+    <script src="js/login.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div class="container">
+    <div id="dashboard">
+
+        <div class="dashboard-item">
+            <img src="images/dash-icons.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+        <div class="dashboard-item">
+            <img src="images/student_access_code.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+        <div class="dashboard-item">
+            <img src="images/file-edit.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+        <div class="dashboard-item">
+            <img src="images/photo_capture.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+
+        <div class="dashboard-item">
+            <img src="images/photo_capture.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+        <div class="dashboard-item">
+            <img src="images/student_access_code.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+        <div class="dashboard-item">
+            <img src="images/file-edit.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+        <div class="dashboard-item">
+            <img src="images/photo_capture.png">
+            <div class="dashboard-desc">Admin Access</div>
+        </div>
+
+
+        <div class="clearfix"></div>
+    </div>
+
+
+</div> <!-- /container -->
+
+</body>
+</html>
