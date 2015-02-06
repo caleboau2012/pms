737b8fd494fdcfaea2e0419e9343d4eb5e32581d
c.mbakwe <caleboau2012@gmail.com>
2015-02-06 10:46:50 +0100

    Removed save changes button. (It's not necessary anymore)

--- admin/staff.php
+++ admin/staff.php
@@ -194,7 +194,7 @@
                             </tr>
                             </tbody>
                         </table>
-                        <button class="btn btn-info pull-right" data-dismiss="modal">Save Changes</button>
+<!--                        <button class="btn btn-info pull-right" data-dismiss="modal">Save Changes</button>-->
                     </div>
                 </div>
             </div>
