fe4424f679df242036e6b8e94caf1e377f1fe3ec
moses adebayo <mos.adebayo@gmail.com>
2015-04-13 10:41:40 +0100

    footer positioning

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-fixed-bottom">
-    <div class="container-fluid">
+<div class="container-fluid">
+    <div id="footer">
         <p class="text-center">
             &copy; 2015. Patient Management System
         </p>
     </div>
-</nav>
+</div>
