ea0771a4a63a5ec1848eb1d37ef28c89eab0713d
olaniyimoses <olaniyi.updates2012@gmail.com>
2015-04-14 14:37:24 +0100

    laboratory

--- view/parasitology.php
+++ view/parasitology.php
@@ -10,133 +10,44 @@ require_once '../_core/global/_require.php';
 
 Crave::requireAll(GLOBAL_VAR);
 Crave::requireAll(UTIL);
+Crave::requireFiles(MODEL, array('BaseModel', 'ChemicalPathologyModel', 'HaematologyModel', 'MicroscopyModel', 'ParasitologyModel', 'VisualModel', 'RadiologyModel'));
+Crave::requireFiles(CONTROLLER, array('LaboratoryController'));
+
 
 if (!isset($_SESSION[UserAuthTable::userid])) {
     header("Location: ../index.php");
 }
+
+$lab = new LaboratoryController();
+$view_bag = array();
+$view_bag = $lab->getLabDetails($_POST['labType'], $_POST['treatment_id']);
+var_dump($view_bag);
 ?>
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Parasitology</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/bootstrap/jquery-ui.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="../css/master.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
 
 <body>
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="dashboard.php">Patient Management System</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right nav-pills">
-                <div class="dropdown navbar-right navbar-right-text pointer">
-                    <span class="dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-                    <img src="../images/profile.png">
-                    <span>
-                        <?php echo ucwords(CxSessionHandler::getItem(ProfileTable::surname) . ' ' . CxSessionHandler::getItem(ProfileTable::firstname)) ?>
-                    </span>
-                    <span class="caret"></span>
-                </span>
-                    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                        <li role="presentation"><a href="#" id="sign-out">Sign out</a></li>
-                    </ul>
-                </div>
-            </ul>
-            <form class="treatment navbar-form">
-                <div class="search form-inline">
-                    <input type="text" class="form-control" name="search" placeholder="Search Returning Patients...">
-                </div>
-            </form>
-        </div>
-    </div>
-</nav>
-
-<script id="tmplPatients" type="text/html">
-    <div class="panel {{status}} patient">
-        <div class="panel-heading" role="tab" id="heading{{patientid}}">
-            <h4 class="panel-title">
-                <a class="collapsed" data-toggle="collapse" data-parent="#accordion{{userid}}"
-                   href="#collapse{{patientid}}" aria-expanded="false" aria-controls="collapse{{patientid}}">
-                    {{regNo}}
-                </a>
-            </h4>
-        </div>
-        <div id="collapse{{patientid}}" class="panel-collapse collapse" role="tabpanel"
-             aria-labelledby="heading{{patientid}}">
-            <div class="panel-body">
-                <p>{{name}}</p>
-
-                <p>{{sex}}</p>
-                <span class="patientid" hidden>{{patientid}}</span>
-                <span class="doctorid" hidden>{{userid}}</span>
-            </div>
-        </div>
-    </div>
-</script>
-
-<script id="tmplDoctor" type="text/html">
-    <div class="col-sm-4 col-md-3">
-        <div class="panel {{online_status}} doctor">
-            <div class="panel-heading" userid="{{userid}}">
-                <h2 class="panel-title">Dr. {{DoctorName}}</h2>
-            </div>
-            <div class="panel-body patients">
-                <span class="to_doctor" hidden>{{userid}}</span>
-
-                <div class="panel-group drop" id="accordion{{userid}}" role="tablist" aria-multiselectable="true">
-                </div>
-            </div>
-        </div>
-    </div>
-</script>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-12 well">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title">{{Patient Name}}</h2>
+                    <h2 class="panel-title"><?php echo $_POST['surname'] .' '. $_POST['firstname'].' '. $_POST['middlename'];  ?></h2>
                 </div>
                 <div class="panel-body">
-                    <p>{{Reg No}}</p>
-                    <span>{{Sex}}</span>
-                    <span>{{Age}} years</span>
+                    <p><?php echo $_POST['regNo']; ?></p>
+                    <span><?php echo $_POST['sex']; ?></span>
+                    <span></span>
                 </div>
             </div>
 
             <div class="parasitology">
                 <div class="add-parasitology">
                     <form id="addTestForm" class="form">
-                        <input type="hidden" name="<?php echo ParasitologyRequestTable::table_name.'['.ParasitologyRequestTable::preq_id.']' ?>" value="treatment_id">
-                        <input type="hidden" name="<?php echo ParasitologyRequestTable::table_name.'['.ParasitologyRequestTable::treatment_id.']'; ?>" value="treatment_id">
-
+                        <input type="hidden" name="<?php echo 'data[details]'.'['.ParasitologyRequestTable::preq_id.']'; ?>" value="<?php echo $view_bag['details']['preq_id'] ?>" />
+                        <input type="hidden" name="<?php  echo 'data[details]'.'['.ParasitologyRequestTable::lab_attendant_id.']' ?>" value="<?php if(isset($view_bag['details']['lab_attendant_id'])) echo $view_bag['details']['lab_attendant_id'] ?>" />
+                        <input type="hidden" name="<?php echo 'data[details'.']['.ParasitologyRequestTable::treatment_id.']'; ?>"  value="<?php echo $view_bag['details']['treatment_id'] ?>">
+                        <input type="hidden" name="intent" value="updateLabDetails">
+                        <input type="hidden" name="labType" value="haematology">
                         <div class="row">
                             <div class="page-header">
                                 <a id="back" href="#" class="btn btn-default btn-sm" style="float: left;margin-right: 10px;margin-top: 5px; margin-left: 20px;">← Go Back</a>
@@ -147,8 +58,12 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                 <div class="center-block">
                                     <fieldset>
                                         <h4 class="title">Clinical Diagnosis</h4>
-                                        <textarea readonly class="col-sm-12 form-control"></textarea>
-                                        <div class="test-label">Requesting Doctor:<span class="pad5 test-label">Date:</span></div>
+                                        <textarea readonly class="col-sm-12 form-control"><?php
+                                            if(isset($view_bag['details']['diagnosis'])){
+                                                echo $view_bag['details']['diagnosis'];
+                                            }
+                                            ?></textarea>
+                                        <div class="test-label">Requesting Doctor:<span class="pad5 test-label">Date: <?php if(isset($view_bag['details']['created_date'])) echo $view_bag['details']['created_date']; ?></span></div>
                                     </fieldset>
                                 </div>
                             </div>
@@ -158,13 +73,21 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                     <div class="col-sm-6">
                                         <fieldset>
                                             <label class="test-label">Nature of Specimen</label>
-                                            <textarea class="col-sm-12 form-control" placeholder="Nature of Specimen" name="<?php echo ParasitologyRequestTable::table_name.'['.ParasitologyRequestTable::nature_of_specimen.']'; ?>"></textarea>
+                                            <textarea class="col-sm-12 form-control" placeholder="Nature of Specimen" name="<?php echo 'data[details]['.ParasitologyRequestTable::nature_of_specimen.']'; ?>"><?php
+                                                if (isset($view_bag['details'][ParasitologyRequestTable::nature_of_specimen])){
+                                                    echo $view_bag['details'][ParasitologyRequestTable::nature_of_specimen];
+                                                }
+                                                ?></textarea>
                                         </fieldset>
                                     </div>
                                     <div class="col-sm-6">
                                         <label class="test-label">Investigation Required</label>
-                                        <textarea class="col-sm-12 form-control" placeholder="Investigation Required" name="<?php echo ParasitologyRequestTable::table_name.'['.ParasitologyRequestTable::investigation_required.']'; ?>"></textarea>
-                                        <div class="test-label">Date of Collection:<span class="pad5 test-label">Date Reported:</span></div>
+                                        <textarea class="col-sm-12 form-control" placeholder="Investigation Required" name="<?php echo 'data[details]['.ParasitologyRequestTable::investigation_required.']'; ?>"><?php
+                                            if (isset($view_bag['details']['investigation_req'])) {
+                                                echo $view_bag['details']['investigation_req'];
+                                            }
+                                            ?></textarea>
+                                        <div class="test-label">Date of Collection: <?php if (isset($view_bag['details']['created_date'])) echo $view_bag['details']['created_date']; ?><span class="pad5 test-label">Date Reported: <?php if (isset($view_bag['details']['date_reported'])) echo $view_bag['details']['date_reported']; ?></span></div>
                                     </div>
                                 </div>
                             </div>
@@ -172,8 +95,12 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                 <div class="center-block">
                                     <fieldset>
                                         <h4 class="title">Laboratory Report</h4>
-                                        <textarea class="col-sm-12 form-control" placeholder="Laboratory Report" name="<?php echo ParasitologyRequestTable::table_name.'['.ParasitologyRequestTable::lab_comment.']'; ?>"></textarea>
-                                        <div class="test-label">Laboratory Ref: <span><input type="text" class="form-inline form-margin" name="<?php echo ParasitologyRequestTable::table_name.'['.ParasitologyRequestTable::lab_number.']'; ?>"></span> </div>
+                                        <textarea class="col-sm-12 form-control" placeholder="Laboratory Report" name="<?php echo 'data[details]['.ParasitologyRequestTable::lab_comment.']'; ?>"><?php
+                                            if (isset($view_bag['details'][ParasitologyRequestTable::lab_comment])){
+                                                echo $view_bag['details'][ParasitologyRequestTable::lab_comment];
+                                            }
+                                            ?></textarea>
+                                        <div class="test-label">Laboratory Ref: <span><input type="text" class="form-inline form-margin" name="<?php echo 'data[details]['.ParasitologyRequestTable::lab_number.']'; ?>" value="<?php if (isset($view_bag['details'][ParasitologyRequestTable::lab_number])) echo $view_bag['details'][ParasitologyRequestTable::lab_number]; ?>"></span> </div>
                                     </fieldset>
                                 </div>
                             </div>
@@ -221,7 +148,7 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                         foreach ($cells_parasites->getList() as $label){ ?>
                                         <tr>
                                             <td class="test-label"><?php echo $label->getLabel(); ?></td>
-                                            <td class="text-center"><input type="checkbox" name="<?php echo ParasitologyDetailsTable::table_name.'['.$label->getId().']'; ?>"></td>
+                                            <td class="text-center"><input type="checkbox" name="<?php echo 'parasites['.$label->getId().']'; ?>" <?php if (isset($view_bag['parasites'][$label->getId()])) {echo "checked";} ?>></td>
                                         </tr>
                                         <?php
                                         }
@@ -243,7 +170,7 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                         ?>
                                         <tr>
                                             <td class="test-label"><?php echo $label->getLabel();?></td>
-                                            <td class="text-center"><input type="checkbox" name="<?php echo  ParasitologyDetailsTable::table_name.'['.$label->getId().']'; ?>"> </td>
+                                            <td class="text-center"><input type="checkbox" name="<?php echo  'parasites['.$label->getId().']'; ?>" <?php if (isset($view_bag['parasites'][$label->getId()])) {echo "checked";} ?>> </td>
                                         </tr>
                                     <?php
                                     }
@@ -265,7 +192,7 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                         ?>
                                         <tr>
                                             <td class="test-label"><?php echo $label->getLabel();?></td>
-                                            <td class="text-center"><input name="<?php echo  ParasitologyDetailsTable::table_name.'['.$label->getId().']'; ?>" type="checkbox"> </td>
+                                            <td class="text-center"><input name="<?php echo  'parasites['.$label->getId().']'; ?>" type="checkbox" <?php if (isset($view_bag['parasites'][$label->getId()])) {echo "checked";} ?>> </td>
                                         </tr>
                                     <?php
                                     }
@@ -287,7 +214,7 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                         ?>
                                         <tr>
                                             <td class="test-label"><?php echo $label->getLabel();?></td>
-                                            <td class="text-center"><input type="checkbox" name="<?php echo  ParasitologyDetailsTable::table_name.'['.$label->getId().']'; ?>"> </td>
+                                            <td class="text-center"><input type="checkbox" name="<?php echo  'parasites['.$label->getId().']'; ?>" <?php if (isset($view_bag['parasites'][$label->getId()])) {echo "checked";} ?>> </td>
                                         </tr>
                                     <?php
                                     }
@@ -300,7 +227,7 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                                     ?>
                                     <tr>
                                         <td class="test-label"><?php echo $label->getLabel();?></td>
-                                        <td class="text-center"><input type="checkbox"  name="<?php echo  ParasitologyDetailsTable::table_name.'['.$label->getId().']'; ?>"> </td>
+                                        <td class="text-center"><input type="checkbox"  name="<?php echo 'parasites['.$label->getId().']'; ?>" <?php if (isset($view_bag['parasites'][$label->getId()])) {echo "checked";} ?>> </td>
                                     </tr>
                                     <?php } ?>
                                     </tbody>
@@ -326,7 +253,5 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
 <script src="../js/bootstrap/bootstrap.min.js"></script>
 <script src="../js/bootstrap/jquery-ui.min.js"></script>
 <script src="../js/constants.js"></script>
-<script src="../js/libs/masonry.js"></script>
-<script src="../js/treatment.js"></script>
+<script src="../js/laboratory.js" type="text/javascript"></script>
 </body>
-</html>
