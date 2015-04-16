9e6be29c476da392cb576280576a8539d7c7dcb5
olaniyimoses <olaniyi.updates2012@gmail.com>
2015-04-16 12:03:34 +0100

    trying to fix laboratory issh

---
+++ _core/global/config.php
@@ -0,0 +1,21 @@
+<?php
+//DATABASE CONFIGURATION
+define('DB_HOST', '10.81.0.26');
+//define('DB_HOST', 'localhost');
+define('DBNAME', 'pms');
+define('DB_USERNAME', 'pms');
+define('DB_PASSWORD', '__$pmsadmin)(--');
+
+//define('DB_HOST', 'localhost');
+//define('DBNAME', 'pms');
+//define('DB_USERNAME', 'admin');
+//define('DB_PASSWORD', 'admin');
+
+//PROJECT CONFIG
+define('PROJECT_NAME', 'pms'); //Change project name here, and in global/_require.php
+
+//FOLDER ALIASES
+define('MODEL', '_core/model/');
+define('GLOBAL_VAR', '_core/global/');
+define('CONTROLLER', '_core/controller/');
+define('UTIL', '_core/util/');
