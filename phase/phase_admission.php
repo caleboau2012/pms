2df6344704d79164efdaed1a60484e625abe9891
Sanni Abdulmusawwir <sanniabdulmusawwir@gmail.com>
2015-05-12 17:17:45 +0100

    Fixed missing require in patient admission code

--- phase/phase_admission.php
+++ phase/phase_admission.php
@@ -4,7 +4,7 @@ require_once '../_core/global/_require.php';
 Crave::requireAll(GLOBAL_VAR);
 Crave::requireAll(UTIL);
 Crave::requireFiles(MODEL, array('BaseModel', 'AdmissionModel', 'RoleModel', 'BedModel', 'WardModel', 'VitalsModel'));
-Crave::requireFiles(CONTROLLER, array('AdmissionController', 'RoleController', 'VitalsController'));
+Crave::requireFiles(CONTROLLER, array('AdmissionController', 'RoleController', 'VitalsController', 'WardController'));
 
 if (isset($_REQUEST['intent'])) {
     $intent = $_REQUEST['intent'];
