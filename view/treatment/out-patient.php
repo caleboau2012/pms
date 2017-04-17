5e12a7020efd2f982e5772e00eb61ecf68a1d5b3
moses adebayo <mos.adebayo@gmail.com>
2017-04-17 15:39:34 +0100

    --Fix update data table records  for lab history --Sort record by date created

--- view/treatment/out-patient.php
+++ view/treatment/out-patient.php
@@ -28,12 +28,18 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
 
     <!-- Custom styles for this template -->
     <link href="../../css/master.css" rel="stylesheet">
-    <link href="../../css/bootstrap/jquery.dataTables.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--Datatable -->
+    <link rel="stylesheet" type="text/css" href="../../js/libs/jqueryDataTable/jquery.dataTable.css"/>
+    <link rel="stylesheet" type="text/css" href="../../js/libs/jqueryDataTable/datatableBootstrap.css"/>    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+        .form-inline div {
+            margin-right: 0;
+        }
+    </style>
 </head>
 
 <body>
@@ -408,7 +414,7 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
                             </select>
                         </div>
                         <br/>
-                        <table class="table table-stripped table--bordered dataTable">
+                        <table class="table table-stripped table--bordered dataTable" id="labHistoryTable">
                             <thead>
                             <tr>
                                 <th>Session ID</th>
@@ -468,8 +474,11 @@ if (!isset($_SESSION[UserAuthTable::userid])) {
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="../../js/bootstrap/jquery-1.10.2.min.js"></script>
-<script src="../../js/bootstrap/jquery.dataTables.min.js"></script>
+<!--<script src="../../js/bootstrap/jquery.dataTables.min.js"></script>-->
 <script src="../../js/bootstrap/bootstrap.min.js"></script>
+<script src="../../js/libs/jqueryDataTable/jquery.datatables.min.js"></script>
+<script src="../../js/libs/jqueryDataTable/datatableResponsive.js"></script>
+<script src="../../js/libs/jqueryDataTable/dataTableBootstrap.js"></script>
 <script src="../../js/bootstrap/jquery-ui.min.js"></script>
 <script src="../../js/libs/bootstrap-notify/bootstrap-notify.min.js"></script>
 <script src="../../js/constants.js"></script>
