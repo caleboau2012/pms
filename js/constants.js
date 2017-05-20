fad737a83a7f176fbe0c59da2512411d1e1bbad4
KayLee <caleboau2012@gmail.com>
2017-05-20 02:48:34 +0100

    Made hostname in js dynamic.
    #kaylee

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 /**
  * Created by user on 1/28/2015.
  */
-var host = "http://localhost/pms/";
+var host = location.protocol + "://" + location.hostname + "/pms/";
 
 function printElem(title, body, footer){
     console.log({
